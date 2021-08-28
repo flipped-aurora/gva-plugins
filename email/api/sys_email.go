287731daeddb904c0260490732b0656cd3eb23a2
蒋吉兆 <303176530@qq.com>
2021-08-28 13:06:49 +0800

    提交email插件

---
+++ email/api/sys_email.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"github.com/flipped-aurora/gin-vue-admin/server/global"
+	"github.com/flipped-aurora/gin-vue-admin/server/model/common/response"
+	"github.com/flipped-aurora/gva-plugins/email/service"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+type EmailApi struct {
+}
+
+// @Tags System
+// @Summary 发送测试邮件
+// @Security ApiKeyAuth
+// @Produce  application/json
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"发送成功"}"
+// @Router /email/emailTest [post]
+func (s *EmailApi) EmailTest(c *gin.Context) {
+	if err := service.ServiceGroupApp.EmailTest(); err != nil {
+		global.GVA_LOG.Error("发送失败!", zap.Any("err", err))
+		response.FailWithMessage("发送失败", c)
+	} else {
+
+		response.OkWithData("发送成功", c)
+	}
+}
---
+++ email/global/gloabl.go
@@ -0,0 +1,5 @@
+package global
+
+import "github.com/flipped-aurora/gva-plugins/email/config"
+
+var GlobalConfig = new(config.Email)
